2ca187f526200ca2979ac6a7c7ef2c9deca788a4
vahkjsdf <vahkjsdf@sookmyung.ac.kr>
2026-06-27 04:19:58 +0900

    [add/#1] NetworkModule 추가

---
+++ app/src/main/java/com/haphap/app/core/network/di/NetworkModule.kt
@@ -0,0 +1,82 @@
+package com.haphap.app.core.network.di
+
+import com.haphap.app.BuildConfig
+import com.haphap.app.core.extensions.isJsonArray
+import com.haphap.app.core.extensions.isJsonObject
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.serialization.json.Json
+import okhttp3.Interceptor
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import org.json.JSONArray
+import org.json.JSONObject
+import retrofit2.Converter
+import retrofit2.Retrofit
+import timber.log.Timber
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+    private const val CONTENT_TYPE = "application/json"
+    private const val LOGGING_TAG = "okhttp"
+
+    @Provides
+    @Singleton
+    fun provideJson(): Json = Json {
+        encodeDefaults = true
+        ignoreUnknownKeys = true
+        prettyPrint = BuildConfig.DEBUG
+    }
+
+    @Provides
+    @Singleton
+    fun provideJsonConverter(json: Json): Converter.Factory =
+        json.asConverterFactory(CONTENT_TYPE.toMediaType())
+
+    @Provides
+    @Singleton
+    fun provideHttpLoggingInterceptor(): Interceptor = HttpLoggingInterceptor { message ->
+        when {
+            message.isJsonObject() ->
+                Timber.Forest.tag(LOGGING_TAG).d(JSONObject(message).toString(4))
+
+            message.isJsonArray() ->
+                Timber.Forest.tag(LOGGING_TAG).d(JSONArray(message).toString(4))
+
+            else -> {
+                Timber.Forest.tag(LOGGING_TAG).d("CONNECTION INFO -> $message")
+            }
+        }
+    }.apply {
+        level = if (BuildConfig.DEBUG) {
+            HttpLoggingInterceptor.Level.BODY
+        } else {
+            HttpLoggingInterceptor.Level.NONE
+        }
+    }
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(
+        loggingInterceptor: Interceptor,
+    ): OkHttpClient = OkHttpClient.Builder()
+        .addInterceptor(loggingInterceptor)
+        .build()
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(
+        client: OkHttpClient,
+        factory: Converter.Factory,
+    ): Retrofit = Retrofit.Builder()
+        .baseUrl(BuildConfig.BASE_URL)
+        .client(client)
+        .addConverterFactory(factory)
+        .build()
+}
