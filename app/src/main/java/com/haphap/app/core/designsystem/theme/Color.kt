90ffd4001b92a05b46d23417bce22b247ad30da0
xancxoxin <judy010321@naver.com>
2026-06-29 20:32:33 +0900

    [feat/#6] 색상 시스템 정의

--- app/src/main/java/com/haphap/app/core/designsystem/theme/Color.kt
+++ app/src/main/java/com/haphap/app/core/designsystem/theme/Color.kt
@@ -2,10 +2,25 @@ package com.haphap.app.core.designsystem.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Primary500 = Color(0xFF0065FF)
+val Primary100 = Color(0xFF4A8EFF)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Sub100 = Color(0xFFE7F1FF)
+val Sub200 = Color(0xFF82B1FF)
+val Sub300 = Color(0xFFC3D9FF)
+val Sub400 = Color(0xFF041B50)
+val Yellow = Color(0xFFFEE500)
+
+val Gray50 = Color(0xFFFAFBFB)
+val Gray100 = Color(0xFFF3F4F7)
+val Gray200 = Color(0xFFD2D6DB)
+val Gray300 = Color(0xFFB2B8C0)
+val Gray400 = Color(0xFF8E95A0)
+val Gray500 = Color(0xFF6D7683)
+val Gray600 = Color(0xFF505867)
+val Gray700 = Color(0xFF181E27)
+val Gray800 = Color(0xFF141920)
+val Gray900 = Color(0xFF111214)
+
+val White = Color(0xFFFFFFFF)
+val Black = Color(0xFF000000)
