38188f1fd397c519d6196edef5adc38f1851981d
xancxoxin <judy010321@naver.com>
2026-06-30 00:46:23 +0900

    [mod/#6] 타이포그래피 카테고리 계층 구조로 변경

--- app/src/main/java/com/haphap/app/core/designsystem/theme/Type.kt
+++ app/src/main/java/com/haphap/app/core/designsystem/theme/Type.kt
@@ -27,82 +27,52 @@ private fun pretendard(
     letterSpacing = 0.sp,
 )
 
-val t_b_30: TextStyle = pretendard(FontWeight.Bold, 30)
-val t_b_26: TextStyle = pretendard(FontWeight.Bold, 26)
-val st_b_24: TextStyle = pretendard(FontWeight.Bold, 24, 1.3f)
-val st_sb_24: TextStyle = pretendard(FontWeight.SemiBold, 24)
-val st_b_22: TextStyle = pretendard(FontWeight.Bold, 22)
-val st_b_20: TextStyle = pretendard(FontWeight.Bold, 20)
-val b_b_18: TextStyle = pretendard(FontWeight.Bold, 18)
-val b_sb_18: TextStyle = pretendard(FontWeight.SemiBold, 18)
-val b_m_18: TextStyle = pretendard(FontWeight.Medium, 18)
-val b_r_18: TextStyle = pretendard(FontWeight.Normal, 18)
-val b_sb_16: TextStyle = pretendard(FontWeight.SemiBold, 16)
-val b_r_16: TextStyle = pretendard(FontWeight.Normal, 16)
-val b_b_14: TextStyle = pretendard(FontWeight.Bold, 14, 1.3f)
-val b_sb_14: TextStyle = pretendard(FontWeight.SemiBold, 14)
-val b_m_14: TextStyle = pretendard(FontWeight.Medium, 14)
-val b_r_14: TextStyle = pretendard(FontWeight.Normal, 14)
-val b_sb_13: TextStyle = pretendard(FontWeight.SemiBold, 13)
-val c_b_12: TextStyle = pretendard(FontWeight.Bold, 12)
-val c_sb_12: TextStyle = pretendard(FontWeight.SemiBold, 12)
-val c_m_12: TextStyle = pretendard(FontWeight.Medium, 12)
-val c_r_12: TextStyle = pretendard(FontWeight.Normal, 12)
-val c_r_11: TextStyle = pretendard(FontWeight.Normal, 11)
-val c_sb_10: TextStyle = pretendard(FontWeight.SemiBold, 10)
-val c_r_10: TextStyle = pretendard(FontWeight.Normal, 10)
+@Immutable
+data class TitleStyle(
+    val b30: TextStyle = pretendard(FontWeight.Bold, 30),
+    val b26: TextStyle = pretendard(FontWeight.Bold, 26),
+)
+
+@Immutable
+data class SubtitleStyle(
+    val b24: TextStyle = pretendard(FontWeight.Bold, 24, 1.3f),
+    val sb24: TextStyle = pretendard(FontWeight.SemiBold, 24),
+    val b22: TextStyle = pretendard(FontWeight.Bold, 22),
+    val b20: TextStyle = pretendard(FontWeight.Bold, 20),
+)
+
+@Immutable
+data class BodyStyle(
+    val b18: TextStyle = pretendard(FontWeight.Bold, 18),
+    val sb18: TextStyle = pretendard(FontWeight.SemiBold, 18),
+    val m18: TextStyle = pretendard(FontWeight.Medium, 18),
+    val r18: TextStyle = pretendard(FontWeight.Normal, 18),
+    val sb16: TextStyle = pretendard(FontWeight.SemiBold, 16),
+    val r16: TextStyle = pretendard(FontWeight.Normal, 16),
+    val b14: TextStyle = pretendard(FontWeight.Bold, 14, 1.3f),
+    val sb14: TextStyle = pretendard(FontWeight.SemiBold, 14),
+    val m14: TextStyle = pretendard(FontWeight.Medium, 14),
+    val r14: TextStyle = pretendard(FontWeight.Normal, 14),
+    val sb13: TextStyle = pretendard(FontWeight.SemiBold, 13),
+)
+
+@Immutable
+data class CaptionStyle(
+    val b12: TextStyle = pretendard(FontWeight.Bold, 12),
+    val sb12: TextStyle = pretendard(FontWeight.SemiBold, 12),
+    val m12: TextStyle = pretendard(FontWeight.Medium, 12),
+    val r12: TextStyle = pretendard(FontWeight.Normal, 12),
+    val r11: TextStyle = pretendard(FontWeight.Normal, 11),
+    val sb10: TextStyle = pretendard(FontWeight.SemiBold, 10),
+    val r10: TextStyle = pretendard(FontWeight.Normal, 10),
+)
 
 @Immutable
 data class HaphapTypography(
-    val t_b_30: TextStyle,
-    val t_b_26: TextStyle,
-    val st_b_24: TextStyle,
-    val st_sb_24: TextStyle,
-    val st_b_22: TextStyle,
-    val st_b_20: TextStyle,
-    val b_b_18: TextStyle,
-    val b_sb_18: TextStyle,
-    val b_m_18: TextStyle,
-    val b_r_18: TextStyle,
-    val b_sb_16: TextStyle,
-    val b_r_16: TextStyle,
-    val b_b_14: TextStyle,
-    val b_sb_14: TextStyle,
-    val b_m_14: TextStyle,
-    val b_r_14: TextStyle,
-    val b_sb_13: TextStyle,
-    val c_b_12: TextStyle,
-    val c_sb_12: TextStyle,
-    val c_m_12: TextStyle,
-    val c_r_12: TextStyle,
-    val c_r_11: TextStyle,
-    val c_sb_10: TextStyle,
-    val c_r_10: TextStyle,
+    val title: TitleStyle = TitleStyle(),
+    val subtitle: SubtitleStyle = SubtitleStyle(),
+    val body: BodyStyle = BodyStyle(),
+    val caption: CaptionStyle = CaptionStyle(),
 )
 
-val defaultHaphapTypography = HaphapTypography(
-    t_b_30 = t_b_30,
-    t_b_26 = t_b_26,
-    st_b_24 = st_b_24,
-    st_sb_24 = st_sb_24,
-    st_b_22 = st_b_22,
-    st_b_20 = st_b_20,
-    b_b_18 = b_b_18,
-    b_sb_18 = b_sb_18,
-    b_m_18 = b_m_18,
-    b_r_18 = b_r_18,
-    b_sb_16 = b_sb_16,
-    b_r_16 = b_r_16,
-    b_b_14 = b_b_14,
-    b_sb_14 = b_sb_14,
-    b_m_14 = b_m_14,
-    b_r_14 = b_r_14,
-    b_sb_13 = b_sb_13,
-    c_b_12 = c_b_12,
-    c_sb_12 = c_sb_12,
-    c_m_12 = c_m_12,
-    c_r_12 = c_r_12,
-    c_r_11 = c_r_11,
-    c_sb_10 = c_sb_10,
-    c_r_10 = c_r_10,
-)
+val defaultHaphapTypography = HaphapTypography()
