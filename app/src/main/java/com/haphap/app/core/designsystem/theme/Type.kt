e30aa1d995222ab9de6711b57570aa5ba7ee7ab4
xancxoxin <judy010321@naver.com>
2026-06-30 01:16:04 +0900

    [mod/#6] 파라미터명 명시

--- app/src/main/java/com/haphap/app/core/designsystem/theme/Type.kt
+++ app/src/main/java/com/haphap/app/core/designsystem/theme/Type.kt
@@ -37,42 +37,42 @@ private fun pretendard(
 
 @Immutable
 data class TitleStyle(
-    val b30: TextStyle = pretendard(FontWeight.Bold, 30),
-    val b26: TextStyle = pretendard(FontWeight.Bold, 26),
+    val b30: TextStyle = pretendard(weight = FontWeight.Bold, size = 30),
+    val b26: TextStyle = pretendard(weight = FontWeight.Bold, size = 26),
 )
 
 @Immutable
 data class SubtitleStyle(
-    val b24: TextStyle = pretendard(FontWeight.Bold, 24, 1.3f),
-    val sb24: TextStyle = pretendard(FontWeight.SemiBold, 24),
-    val b22: TextStyle = pretendard(FontWeight.Bold, 22),
-    val b20: TextStyle = pretendard(FontWeight.Bold, 20),
+    val b24: TextStyle = pretendard(weight = FontWeight.Bold, size = 24, lineHeightPercent = 1.3f),
+    val sb24: TextStyle = pretendard(weight = FontWeight.SemiBold, size = 24),
+    val b22: TextStyle = pretendard(weight = FontWeight.Bold, size = 22),
+    val b20: TextStyle = pretendard(weight = FontWeight.Bold, size = 20),
 )
 
 @Immutable
 data class BodyStyle(
-    val b18: TextStyle = pretendard(FontWeight.Bold, 18),
-    val sb18: TextStyle = pretendard(FontWeight.SemiBold, 18),
-    val m18: TextStyle = pretendard(FontWeight.Medium, 18),
-    val r18: TextStyle = pretendard(FontWeight.Normal, 18),
-    val sb16: TextStyle = pretendard(FontWeight.SemiBold, 16),
-    val r16: TextStyle = pretendard(FontWeight.Normal, 16),
-    val b14: TextStyle = pretendard(FontWeight.Bold, 14, 1.3f),
-    val sb14: TextStyle = pretendard(FontWeight.SemiBold, 14),
-    val m14: TextStyle = pretendard(FontWeight.Medium, 14),
-    val r14: TextStyle = pretendard(FontWeight.Normal, 14),
-    val sb13: TextStyle = pretendard(FontWeight.SemiBold, 13),
+    val b18: TextStyle = pretendard(weight = FontWeight.Bold, size = 18),
+    val sb18: TextStyle = pretendard(weight = FontWeight.SemiBold, size = 18),
+    val m18: TextStyle = pretendard(weight = FontWeight.Medium, size = 18),
+    val r18: TextStyle = pretendard(weight = FontWeight.Normal, size = 18),
+    val sb16: TextStyle = pretendard(weight = FontWeight.SemiBold, size = 16),
+    val r16: TextStyle = pretendard(weight = FontWeight.Normal, size = 16),
+    val b14: TextStyle = pretendard(weight = FontWeight.Bold, size = 14, lineHeightPercent = 1.3f),
+    val sb14: TextStyle = pretendard(weight = FontWeight.SemiBold, size = 14),
+    val m14: TextStyle = pretendard(weight = FontWeight.Medium, size = 14),
+    val r14: TextStyle = pretendard(weight = FontWeight.Normal, size = 14),
+    val sb13: TextStyle = pretendard(weight = FontWeight.SemiBold, size = 13),
 )
 
 @Immutable
 data class CaptionStyle(
-    val b12: TextStyle = pretendard(FontWeight.Bold, 12),
-    val sb12: TextStyle = pretendard(FontWeight.SemiBold, 12),
-    val m12: TextStyle = pretendard(FontWeight.Medium, 12),
-    val r12: TextStyle = pretendard(FontWeight.Normal, 12),
-    val r11: TextStyle = pretendard(FontWeight.Normal, 11),
-    val sb10: TextStyle = pretendard(FontWeight.SemiBold, 10),
-    val r10: TextStyle = pretendard(FontWeight.Normal, 10),
+    val b12: TextStyle = pretendard(weight = FontWeight.Bold, size = 12),
+    val sb12: TextStyle = pretendard(weight = FontWeight.SemiBold, size = 12),
+    val m12: TextStyle = pretendard(weight = FontWeight.Medium, size = 12),
+    val r12: TextStyle = pretendard(weight = FontWeight.Normal, size = 12),
+    val r11: TextStyle = pretendard(weight = FontWeight.Normal, size = 11),
+    val sb10: TextStyle = pretendard(weight = FontWeight.SemiBold, size = 10),
+    val r10: TextStyle = pretendard(weight = FontWeight.Normal, size = 10),
 )
 
 @Immutable
