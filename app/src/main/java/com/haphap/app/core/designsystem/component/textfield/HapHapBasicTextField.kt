4e017efc01faa83aaed5d9a96289aaad920a2004
vahkjsdf <vahkjsdf@sookmyung.ac.kr>
2026-07-01 03:31:15 +0900

    [mod/#11] placeholder 중앙 정렬 추가
    - placeholder와 inputText 크기 차이로 인한 여백 맞추기용

--- app/src/main/java/com/haphap/app/core/designsystem/component/textfield/HapHapBasicTextField.kt
+++ app/src/main/java/com/haphap/app/core/designsystem/component/textfield/HapHapBasicTextField.kt
@@ -1,9 +1,11 @@
 package com.haphap.app.core.designsystem.component.textfield
 
+import androidx.compose.foundation.border
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.text.input.InputTransformation
@@ -14,11 +16,13 @@ import androidx.compose.foundation.text.input.TextFieldState
 import androidx.compose.foundation.text.input.rememberTextFieldState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.haphap.app.core.designsystem.theme.HapHapTheme
 
 /**
@@ -57,7 +61,7 @@ fun HapHapBasicTextField(
     isReadOnly: Boolean = false,
     keyboardOptions: KeyboardOptions = KeyboardOptions.Default,
     onKeyboardAction: KeyboardActionHandler? = null,
-    lineLimits: TextFieldLineLimits = TextFieldLineLimits.Default,
+    lineLimits: TextFieldLineLimits = TextFieldLineLimits.SingleLine,
     inputTransformation: InputTransformation? = null,
     outputTransformation: OutputTransformation? = null,
     interactionSource: MutableInteractionSource? = null,
@@ -83,6 +87,7 @@ fun HapHapBasicTextField(
             ){
                 Box(
                     modifier = Modifier.weight(1f),
+                    contentAlignment = Alignment.CenterStart
                 ){
                     if (state.text.isEmpty()) {
                         Text(
@@ -99,7 +104,7 @@ fun HapHapBasicTextField(
     )
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
 private fun HapHapBasicTextFieldPreview() {
     val state = rememberTextFieldState(initialText = "")
