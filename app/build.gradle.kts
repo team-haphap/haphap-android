40be65387c2f2327610240bc14d531abd28a39c7
Hiimynameiss <sh2410152@sookmyung.ac.kr>
2026-06-30 21:40:54 +0900

    [feat/#5] 리다이렉트 URI용 액티비티 등록

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,6 +14,8 @@ val properties = Properties().apply {
     load(project.rootProject.file("local.properties").inputStream())
 }
 
+val nativeAppKey = properties.getProperty("native.app.key").removeSurrounding("\"")
+
 android {
     namespace = "com.haphap.app"
     compileSdk {
@@ -34,6 +36,7 @@ android {
         buildConfigField("String", "BASE_URL", properties.getProperty("base.url"))
         buildConfigField("String", "NATIVE_APP_KEY", properties.getProperty("native.app.key"))
 
+        manifestPlaceholders["nativeAppKey"] = nativeAppKey
     }
 
     buildTypes {
