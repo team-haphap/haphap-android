b2af61cff140c5e852623f7fbb4fc2b728fed56b
vahkjsdf <vahkjsdf@sookmyung.ac.kr>
2026-07-01 02:55:56 +0900

    [del/#11] 불필요한 import 제거

--- app/src/main/java/com/haphap/app/core/designsystem/component/searchbar/HapHapSearchBar.kt
+++ app/src/main/java/com/haphap/app/core/designsystem/component/searchbar/HapHapSearchBar.kt
@@ -1,7 +1,6 @@
 package com.haphap.app.core.designsystem.component.searchbar
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
