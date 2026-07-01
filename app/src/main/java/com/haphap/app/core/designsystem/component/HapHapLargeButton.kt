93391bc552406ca7cc41755955b2c8e4223dd930
Hiimynameiss <sh2410152@sookmyung.ac.kr>
2026-07-02 02:16:35 +0900

    [del/#18] btn_cta_l에서 쓸모없는 import문 삭제

--- app/src/main/java/com/haphap/app/core/designsystem/component/HapHapLargeButton.kt
+++ app/src/main/java/com/haphap/app/core/designsystem/component/HapHapLargeButton.kt
@@ -2,14 +2,12 @@ package com.haphap.app.core.designsystem.component
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
