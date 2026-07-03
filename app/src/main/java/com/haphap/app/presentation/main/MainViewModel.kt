d53a001bca7684b5ed54b70a572957928f644faa
Hiimynameiss <sh2410152@sookmyung.ac.kr>
2026-07-03 21:27:41 +0900

    [mod/#5] 시작점을 로그인 화면으로 고정

--- app/src/main/java/com/haphap/app/presentation/main/MainViewModel.kt
+++ app/src/main/java/com/haphap/app/presentation/main/MainViewModel.kt
@@ -3,9 +3,7 @@ package com.haphap.app.presentation.main
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.haphap.app.core.navigation.Route
-import com.haphap.app.data.local.datasource.api.LocalTokenDataSource
 import com.haphap.app.presentation.auth.navigation.Login
-import com.haphap.app.presentation.home.navigation.Home
 import dagger.hilt.android.lifecycle.HiltViewModel
 import jakarta.inject.Inject
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -14,17 +12,14 @@ import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 
 @HiltViewModel
-class MainViewModel @Inject constructor(
-    private val localTokenDataSource: LocalTokenDataSource,
-) : ViewModel() {
+class MainViewModel @Inject constructor() : ViewModel() {
 
     private val _startDestination = MutableStateFlow<Route?>(null)
     val startDestination: StateFlow<Route?> = _startDestination.asStateFlow()
 
     init {
         viewModelScope.launch {
-            _startDestination.value =
-                if (localTokenDataSource.getAccessToken() != null) Home else Login
+            _startDestination.value = Login
         }
     }
 }
