a6b0b78de0d296c121612b8695259fdac53509d2
Hiimynameiss <sh2410152@sookmyung.ac.kr>
2026-07-04 01:53:55 +0900

    [feat/#5] AuthService 구현

---
+++ app/src/main/java/com/haphap/app/data/remote/service/AuthService.kt
@@ -0,0 +1,14 @@
+package com.haphap.app.data.remote.service
+
+import com.haphap.app.data.remote.dto.BaseResponse
+import com.haphap.app.data.remote.dto.KakaoLoginRequestDto
+import com.haphap.app.data.remote.dto.KakaoLoginResponseDto
+import retrofit2.http.Body
+import retrofit2.http.POST
+
+interface AuthService {
+    @POST("api/v1/auth/kakao")
+    suspend fun kakaoLogin(
+        @Body request: KakaoLoginRequestDto,
+    ): BaseResponse<KakaoLoginResponseDto>
+}
