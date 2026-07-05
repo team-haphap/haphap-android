47e440308fe7c8130cb6749bdcff543fc53cc2e5
Hiimynameiss <sh2410152@sookmyung.ac.kr>
2026-07-05 14:00:14 +0900

    [add/#5] Dto에 @SerialName 추가

--- app/src/main/java/com/haphap/app/data/remote/dto/BaseResponse.kt
+++ app/src/main/java/com/haphap/app/data/remote/dto/BaseResponse.kt
@@ -1,11 +1,16 @@
 package com.haphap.app.data.remote.dto
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class BaseResponse<T>(
+    @SerialName("status")
     val status: Int,
+    @SerialName("code")
     val code: String,
+    @SerialName("message")
     val message: String,
+    @SerialName("data")
     val data: T? = null,
 )
--- app/src/main/java/com/haphap/app/data/remote/dto/auth/KakaoLoginResponseDto.kt
+++ app/src/main/java/com/haphap/app/data/remote/dto/auth/KakaoLoginResponseDto.kt
@@ -1,11 +1,16 @@
 package com.haphap.app.data.remote.dto.auth
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class KakaoLoginResponseDto(
+    @SerialName("accessToken")
     val accessToken: String,
+    @SerialName("refreshToken")
     val refreshToken: String,
+    @SerialName("name")
     val name: String,
+    @SerialName("anonymousName")
     val anonymousName: String,
 )
