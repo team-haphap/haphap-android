281c4ea4e0848acc05319e3cfc29b66934eb47de
Hiimynameiss <sh2410152@sookmyung.ac.kr>
2026-07-06 02:46:31 +0900

    [del/#5] 불필요한 import문 제거

--- app/src/main/java/com/haphap/app/presentation/auth/SignUpCompleteScreen.kt
+++ app/src/main/java/com/haphap/app/presentation/auth/SignUpCompleteScreen.kt
@@ -2,7 +2,6 @@ package com.haphap.app.presentation.auth
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
