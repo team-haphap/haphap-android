fd97f33214dee9102c0d614be3ad504ac72d95f4
chanmi <cksal1052@ewha.ac.kr>
2026-07-06 14:21:44 +0900

    [mod/#28] 코드리뷰 반영 (지민언니)

--- app/src/main/java/com/haphap/app/presentation/mypage/MyPageScreen.kt
+++ app/src/main/java/com/haphap/app/presentation/mypage/MyPageScreen.kt
@@ -6,9 +6,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.haphap.app.core.designsystem.theme.HapHapTheme
 import com.haphap.app.presentation.mypage.component.MyPageProfileCard
 import com.haphap.app.presentation.mypage.component.MyPageTopSection
@@ -16,12 +20,12 @@ import com.haphap.app.presentation.mypage.component.MyPageTopSection
 @Composable
 fun MyPageRoute(
     modifier: Modifier = Modifier,
+    viewModel: MyPageViewModel = hiltViewModel(),
 ) {
+    val uiState by viewModel.uiState.collectAsState()
+
     MyPageScreen(
-        uiState = MyPageUiState(
-            nickNameText = "익명의죠르디",
-            emailText = "yeonsoo1234@naver.com",
-        ),
+        uiState = uiState,
         modifier = modifier,
     )
 }
--- app/src/main/java/com/haphap/app/presentation/mypage/MyPageViewModel.kt
+++ app/src/main/java/com/haphap/app/presentation/mypage/MyPageViewModel.kt
@@ -2,11 +2,20 @@ package com.haphap.app.presentation.mypage
 
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import javax.inject.Inject
 
 @HiltViewModel
 class MyPageViewModel @Inject constructor(
 
 ) : ViewModel() {
-
+    private val _uiState = MutableStateFlow(
+        MyPageUiState(
+            nickNameText = "익명의죠르디",
+            emailText = "yeonsoo1234@naver.com",
+        ),
+    )
+    val uiState: StateFlow<MyPageUiState> = _uiState.asStateFlow()
 }
