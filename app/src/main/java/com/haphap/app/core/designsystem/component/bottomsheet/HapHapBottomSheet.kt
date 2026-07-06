d3beb0ef8218454bbdf7d4290ab8d94336267fb2
chanmi <cksal1052@ewha.ac.kr>
2026-07-07 05:33:06 +0900

    [mod/#14] scrimColor 설정

--- app/src/main/java/com/haphap/app/core/designsystem/component/bottomsheet/HapHapBottomSheet.kt
+++ app/src/main/java/com/haphap/app/core/designsystem/component/bottomsheet/HapHapBottomSheet.kt
@@ -15,6 +15,7 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.material3.SheetState
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -22,6 +23,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
@@ -64,7 +66,7 @@ fun HapHapBottomSheet(
         sheetGesturesEnabled = false,
         shape = RoundedCornerShape(topStart = 16.dp, topEnd = 16.dp),
         containerColor = HapHapTheme.colors.white,
-        // todo scrimColor = ,
+        scrimColor = HapHapTheme.colors.grayBg,
         dragHandle = null,
     ) {
         Column(
