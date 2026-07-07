cc471d2e572afed818a894872582f9f0ff75d44d
vahkjsdf <vahkjsdf@sookmyung.ac.kr>
2026-07-08 06:26:49 +0900

    [feat/#30] toast 메세지 띄우기 구현

--- app/src/main/java/com/haphap/app/presentation/main/MainScreen.kt
+++ app/src/main/java/com/haphap/app/presentation/main/MainScreen.kt
@@ -2,14 +2,30 @@ package com.haphap.app.presentation.main
 
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.haphap.app.core.designsystem.component.toast.HapHapToast
+import com.haphap.app.core.designsystem.component.toast.LocalToastTrigger
 import com.haphap.app.presentation.main.component.MainBottomBar
 import com.haphap.app.presentation.main.component.MainTab
 import kotlinx.collections.immutable.toPersistentList
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
+
+private const val TOAST_DURATION = 2000L
 
 @Composable
 fun MainScreen(
@@ -18,22 +34,78 @@ fun MainScreen(
     val currentTab by appState.currentTab.collectAsStateWithLifecycle()
     val isBottomBarVisible by appState.isBottomBarVisible.collectAsStateWithLifecycle()
 
-    Scaffold(
-        modifier = Modifier.fillMaxSize(),
-        bottomBar = {
-            MainBottomBar(
-                isVisible = isBottomBarVisible,
-                tabs = MainTab.entries.toPersistentList(),
-                currentTab = currentTab,
-                onTabSelected = appState::navigate,
-                modifier = Modifier.navigationBarsPadding()
-            )
-        },
-    ) { innerPadding ->
+    val snackbarHostState = remember { SnackbarHostState() }
+    val coroutineScope = rememberCoroutineScope()
 
-        MainNavHost(
-            appState = appState,
-            innerPadding = innerPadding,
-        )
+    val mutex = remember { Mutex() }
+
+    // 마지막 토스트만 띄우기
+    val onShowToast: (String) -> Unit = { message ->
+        coroutineScope.launch {
+            if (!mutex.tryLock()) return@launch
+
+            try {
+                launch {
+                    delay(TOAST_DURATION)
+                    snackbarHostState.currentSnackbarData?.dismiss()
+                }
+
+                snackbarHostState.showSnackbar(
+                    message = message,
+                    duration = SnackbarDuration.Indefinite,
+                )
+            } finally {
+                mutex.unlock()
+            }
+        }
+    }
+
+    // 모든 토스트 다 띄우기
+    val onShowToast2: (String) -> Unit = { message ->
+        coroutineScope.launch {
+            mutex.withLock {
+                launch {
+                    delay(TOAST_DURATION)
+                    snackbarHostState.currentSnackbarData?.dismiss()
+                }
+
+                snackbarHostState.showSnackbar(
+                    message = message,
+                    duration = SnackbarDuration.Indefinite,
+                )
+            }
+        }
+    }
+
+    CompositionLocalProvider(
+        LocalToastTrigger provides onShowToast,
+    ) {
+        Scaffold(
+            modifier = Modifier.fillMaxSize(),
+            bottomBar = {
+                MainBottomBar(
+                    isVisible = isBottomBarVisible,
+                    tabs = MainTab.entries.toPersistentList(),
+                    currentTab = currentTab,
+                    onTabSelected = appState::navigate,
+                    modifier = Modifier.navigationBarsPadding()
+                )
+            },
+            snackbarHost = {
+                SnackbarHost(hostState = snackbarHostState) { data ->
+                    HapHapToast(
+                        text = data.visuals.message,
+                        modifier = Modifier.padding(20.dp)
+                    )
+                }
+            },
+        ) { innerPadding ->
+
+            MainNavHost(
+                appState = appState,
+                innerPadding = innerPadding,
+            )
+        }
     }
 }
+
