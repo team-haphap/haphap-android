f32a63b04f16f0592ff95dc1f6f97ba374126975
vahkjsdf <vahkjsdf@sookmyung.ac.kr>
2026-07-08 02:38:49 +0900

    [mod/#31] 카드 사이즈 Max 지정

--- app/src/main/java/com/haphap/app/presentation/search/component/SearchDefaultSection.kt
+++ app/src/main/java/com/haphap/app/presentation/search/component/SearchDefaultSection.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.HorizontalDivider
@@ -99,7 +100,7 @@ fun SearchDefaultSection(
                     company = it.company,
                     description = it.description,
                     onCardClick = onCardClick,
-                    modifier = Modifier.width(186.dp),
+                    modifier = Modifier.widthIn(max = 186.dp)
                 )
             }
         }
