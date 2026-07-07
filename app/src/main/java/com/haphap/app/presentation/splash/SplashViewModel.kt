3f1a852a91bd1e52e0d66ec7e7d02c7574009500
Hiimynameiss <sh2410152@sookmyung.ac.kr>
2026-07-07 22:14:50 +0900

    [feat/#37] 스플래시 화면 ViewModel 구현

---
+++ app/src/main/java/com/haphap/app/presentation/splash/SplashViewModel.kt
@@ -0,0 +1,36 @@
+package com.haphap.app.presentation.splash
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.haphap.app.data.local.datasource.api.LocalTokenDataSource
+import dagger.hilt.android.lifecycle.HiltViewModel
+import jakarta.inject.Inject
+import kotlinx.coroutines.async
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+
+@HiltViewModel
+class LoginViewModel @Inject constructor(
+    private val localTokenDataSource: LocalTokenDataSource
+) : ViewModel() {
+    private val _isLoggedIn = MutableStateFlow<Boolean?>(null)
+    val isLoggedIn: StateFlow<Boolean?> = _isLoggedIn
+
+    init {
+        checkLoginState()
+    }
+
+    private fun checkLoginState() {
+        viewModelScope.launch {
+            val tokenDelayed = async { localTokenDataSource.getAccessToken() }
+            val minDelay = async { delay(2000) }
+
+            val accessToken = tokenDelayed.await()
+            minDelay.await()
+
+            _isLoggedIn.value = !accessToken.isNullOrBlank()
+        }
+    }
+}
