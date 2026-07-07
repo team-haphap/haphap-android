1e52e4133772a6518fb5a7f60f24ee715aa951e5
jiyoung2ee <mmuscat33@dgu.ac.kr>
2026-07-07 14:25:40 +0900

    [mod/#32] 프리뷰 패딩 추가

--- app/src/main/java/com/haphap/app/presentation/home/component/HomeListCardTitle.kt
+++ app/src/main/java/com/haphap/app/presentation/home/component/HomeListCardTitle.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -33,7 +34,9 @@ fun HomeListCardTitle(modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 private fun HomeListCardTitlePreview() {
-    HapHapTheme {
-        HomeListCardTitle()
+    Column(modifier = Modifier.padding(horizontal = 20.dp, vertical = 12.dp)) {
+        HapHapTheme {
+            HomeListCardTitle()
+        }
     }
 }
