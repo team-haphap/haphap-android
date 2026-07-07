6ffd6c81712ee51e9351df470b4048a0f145b3be
vahkjsdf <vahkjsdf@sookmyung.ac.kr>
2026-07-07 13:11:18 +0900

    [feat/#31] 검색 완료 화면 구현

---
+++ app/src/main/java/com/haphap/app/presentation/search/component/SearchCompleteComponent.kt
@@ -0,0 +1,175 @@
+package com.haphap.app.presentation.search.component
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.haphap.app.R
+import com.haphap.app.core.designsystem.component.card.HapHapCard
+import com.haphap.app.core.designsystem.component.chip.FilterChipContent
+import com.haphap.app.core.designsystem.component.chip.HapHapFilterChip
+import com.haphap.app.core.designsystem.theme.HapHapTheme
+import com.haphap.app.core.designsystem.type.CardType
+import com.haphap.app.data.model.search.ChipListModel
+import com.haphap.app.data.model.search.JobListModel
+import kotlinx.collections.immutable.ImmutableList
+import kotlinx.collections.immutable.persistentListOf
+
+
+@Composable
+fun SearchCompleteComponent(
+    chipList: ImmutableList<ChipListModel>,
+    selectedChips: ImmutableList<String>,
+    onFilterClick: (String) -> Unit,
+    jobList: ImmutableList<JobListModel>,
+    onCardClick: (Int) -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    Column(
+        modifier = modifier.padding(horizontal = 20.dp),
+    ) {
+        LazyRow(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            item {
+                HapHapFilterChip(
+                    content = FilterChipContent.IconContent(
+                        iconRes = R.drawable.ic_filter_20,
+                        iconResTint = HapHapTheme.colors.gray500,
+                    ),
+                    onFilterClick = {},
+                    isFilterSelected = false
+                )
+
+                Spacer(modifier = Modifier.width(8.dp))
+            }
+
+            items(
+                items = chipList,
+                key = { it.id },
+            ) {
+                HapHapFilterChip(
+                    content = FilterChipContent.TextContent(it.category),
+                    onFilterClick = { onFilterClick(it.category) },
+                    isFilterSelected = selectedChips.contains(it.category),
+                )
+
+                Spacer(modifier = Modifier.width(8.dp))
+            }
+        }
+
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(2),
+            contentPadding = PaddingValues(vertical = 12.dp),
+        ) {
+            items(
+                items = jobList,
+                key = { it.id }
+            ){
+                HapHapCard(
+                    type = CardType.SMALL,
+                    imageUrl = it.imageUrl,
+                    text = it.category,
+                    stage = it.stage,
+                    dDay = it.dDay,
+                    company = it.title,
+                    description = it.content,
+                    onCardClick = { onCardClick(it.id) },
+                )
+            }
+        }
+    }
+}
+
+@Preview (showBackground = true)
+@Composable
+private fun SearchCompleteComponentPreview() {
+    HapHapTheme {
+        var selectedChips by remember { mutableStateOf(persistentListOf("전체")) }
+
+        SearchCompleteComponent(
+            chipList = persistentListOf(
+                ChipListModel(
+                    id = 1,
+                    category = "전체",
+                ),
+                ChipListModel(
+                    id = 2,
+                    category = "개발",
+                ),
+                ChipListModel(
+                    id = 3,
+                    category = "기획/PM",
+                ),
+                ChipListModel(
+                    id = 4,
+                    category = "디자인",
+                ),
+            ),
+            selectedChips = selectedChips,
+            onFilterClick = { category ->
+                selectedChips = if (category == "전체") {
+                    persistentListOf("전체")
+                } else if (selectedChips.contains(category)) {
+                    selectedChips.remove(category)
+                } else {
+                    selectedChips.remove("전체").add(category)
+                }
+            },
+            jobList = persistentListOf(
+                JobListModel(
+                    id = 1,
+                    imageUrl = "",
+                    category = "개발",
+                    stage = "서류",
+                    dDay = 2,
+                    title = "카카오",
+                    content = "공고 설명",
+                ),
+                JobListModel(
+                    id = 2,
+                    imageUrl = "",
+                    category = "개발",
+                    stage = "서류",
+                    dDay = 2,
+                    title = "카카오",
+                    content = "공고 설명",
+                ),
+                JobListModel(
+                    id = 3,
+                    imageUrl = "",
+                    category = "개발",
+                    stage = "서류",
+                    dDay = 2,
+                    title = "카카오",
+                    content = "공고 설명",
+                ),
+                JobListModel(
+                    id = 4,
+                    imageUrl = "",
+                    category = "개발",
+                    stage = "서류",
+                    dDay = 2,
+                    title = "카카오",
+                    content = "공고 설명",
+                ),
+            ),
+            onCardClick = {},
+        )
+    }
+}
