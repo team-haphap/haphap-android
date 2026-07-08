ba54eab903460d9a406a57c6d2b08df837cb3de6
jiyoung2ee <mmuscat33@dgu.ac.kr>
2026-07-08 22:38:37 +0900

    [ui/#48] HomeListCardSection 구현

---
+++ app/src/main/java/com/haphap/app/presentation/home/component/HomeListCardSection.kt
@@ -0,0 +1,79 @@
+package com.haphap.app.presentation.home.component
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.haphap.app.core.designsystem.theme.HapHapTheme
+import com.haphap.app.data.model.home.TodayExpectedCardModel
+import kotlinx.collections.immutable.ImmutableList
+import kotlinx.collections.immutable.persistentListOf
+
+@Composable
+fun HomeListCardSection(
+    todayExpectedCardList: ImmutableList<TodayExpectedCardModel>,
+    onCardClick: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(horizontal = 20.dp)
+    ) {
+        todayExpectedCardList
+            .take(3)
+            .forEach {
+            HomeListCardComponent(
+                imageUrl = it.imageUrl,
+                title = it.title,
+                companyName = it.companyName,
+                category = it.category,
+                stageName = it.stageName,
+                onCardClick = onCardClick,
+            )
+
+            Spacer(modifier = Modifier.height(12.dp))
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun HomeListCardSectionPreview() {
+    HapHapTheme {
+        HomeListCardSection(
+            todayExpectedCardList = persistentListOf(
+                TodayExpectedCardModel(
+                    id = 1,
+                    imageUrl = "",
+                    companyName = "카카오",
+                    category = "개발/데이터",
+                    stageName = "전형",
+                    title = "2026 신입 공개채용",
+                ),
+                TodayExpectedCardModel(
+                    id = 2,
+                    imageUrl = "",
+                    companyName = "카카오",
+                    category = "개발/데이터",
+                    stageName = "전형",
+                    title = "2026 신입 공개채용",
+                ),
+                TodayExpectedCardModel(
+                    id = 3,
+                    imageUrl = "",
+                    companyName = "카카오",
+                    category = "개발/데이터",
+                    stageName = "전형",
+                    title = "2026 신입 공개채용",
+                ),
+            ),
+            onCardClick = {},
+        )
+    }
+}
