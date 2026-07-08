564e7a8a521f5f9d2cf4d19c15a7e896f61f2787
jiyoung2ee <mmuscat33@dgu.ac.kr>
2026-07-08 22:40:45 +0900

    [del/#48] 불필요한 import 구현

--- app/src/main/java/com/haphap/app/presentation/home/HomeViewModel.kt
+++ app/src/main/java/com/haphap/app/presentation/home/HomeViewModel.kt
@@ -5,7 +5,6 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
-import org.jetbrains.annotations.Contract
 import javax.inject.Inject
 
 @HiltViewModel
