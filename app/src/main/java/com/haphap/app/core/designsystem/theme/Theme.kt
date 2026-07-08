ef04255f3824e04c37a661a1f752db5a59babb7e
chanmi <cksal1052@ewha.ac.kr>
2026-07-09 05:36:00 +0900

    [feat/#35] 시스템바 색상 구현

--- app/src/main/java/com/haphap/app/core/designsystem/theme/Theme.kt
+++ app/src/main/java/com/haphap/app/core/designsystem/theme/Theme.kt
@@ -1,13 +1,24 @@
 package com.haphap.app.core.designsystem.theme
 
+import android.app.Activity
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.staticCompositionLocalOf
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
 private val LocalHapHapColors = staticCompositionLocalOf { defaultHapHapColors }
 private val LocalHapHapTypography = staticCompositionLocalOf { defaultHapHapTypography }
+
+private val HapHapColorScheme = lightColorScheme(
+    primary = Primary500,
+    background = White,
+    surface = White,
+)
 
 object HapHapTheme {
     val colors: HapHapColors
@@ -29,6 +40,9 @@ fun HapHapTheme(
         LocalHapHapColors provides defaultHapHapColors,
         LocalHapHapTypography provides defaultHapHapTypography,
     ) {
-        MaterialTheme(content = content)
+        MaterialTheme(
+            colorScheme = HapHapColorScheme,
+            content = content,
+        )
     }
 }
