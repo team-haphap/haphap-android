90138f81142a8508f02f191e06dbe18f4b2ed34a
xancxoxin <judy010321@naver.com>
2026-07-09 02:28:01 +0900

    [mod/#34] 실시간 제보 등록 아이템 -> 불필요한 import 제거

--- app/src/main/java/com/haphap/app/presentation/jobdetail/component/JobStepReportItem.kt
+++ app/src/main/java/com/haphap/app/presentation/jobdetail/component/JobStepReportItem.kt
@@ -1,7 +1,6 @@
 package com.haphap.app.presentation.jobdetail.component
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -9,7 +8,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
