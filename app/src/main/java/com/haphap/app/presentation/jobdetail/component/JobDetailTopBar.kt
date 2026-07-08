1a4da6986455ba47a680e715c67af6d2c9dc6a39
xancxoxin <judy010321@naver.com>
2026-07-08 16:08:11 +0900

    [mod/#34] surface 제거

--- app/src/main/java/com/haphap/app/presentation/jobdetail/component/JobDetailTopBar.kt
+++ app/src/main/java/com/haphap/app/presentation/jobdetail/component/JobDetailTopBar.kt
@@ -1,14 +1,12 @@
 package com.haphap.app.presentation.jobdetail.component
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Icon
-import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -28,51 +26,46 @@ fun JobDetailTopBar(
     modifier: Modifier = Modifier,
     isAlarmActive: Boolean = false,
 ) {
-    Surface(
-        modifier = modifier
+    Row(
+        modifier = Modifier
             .fillMaxWidth()
+            .height(44.dp)
             .background(HapHapTheme.colors.white)
-            .height(44.dp),
-        color = HapHapTheme.colors.white
+            .padding(start = 12.dp),
+        verticalAlignment = Alignment.CenterVertically,
     ) {
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(start = 12.dp),
-            verticalAlignment = Alignment.CenterVertically,
-        ) {
-            val alarmIconRes = if (isAlarmActive) {
-                R.drawable.ic_top_bar_alarm_selected_44
-            } else {
-                R.drawable.ic_top_bar_alarm_default_44
-            }
+        val alarmIconRes = if (isAlarmActive) {
+            R.drawable.ic_top_bar_alarm_selected_44
+        } else {
+            R.drawable.ic_top_bar_alarm_default_44
+        }
 
+        Icon(
+            imageVector = ImageVector.vectorResource(id = R.drawable.ic_back_30),
+            contentDescription = "뒤로가기",
+            modifier = Modifier.noRippleClickable(onClick = { onBackClick() })
+        )
+
+        Spacer(modifier = Modifier.weight(1f))
+
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
             Icon(
-                imageVector = ImageVector.vectorResource(id = R.drawable.ic_back_30),
-                contentDescription = "뒤로가기",
-                modifier = Modifier.noRippleClickable (onClick = { onBackClick() })
+                imageVector = ImageVector.vectorResource(id = alarmIconRes),
+                contentDescription = "알림",
+                modifier = Modifier.noRippleClickable(onClick = { onAlarmClick() })
             )
 
-            Spacer(modifier = Modifier.weight(1f))
-
-            Row(
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Icon(
-                    imageVector = ImageVector.vectorResource(id = alarmIconRes),
-                    contentDescription = "알림",
-                    modifier = Modifier.noRippleClickable(onClick = { onAlarmClick() })
-                )
-
-                Icon(
-                    imageVector = ImageVector.vectorResource(id = R.drawable.ic_top_bar_more_44),
-                    contentDescription = "더보기",
-                    modifier = Modifier.noRippleClickable (onClick ={ onMoreClick() })
-                )
-            }
+            Icon(
+                imageVector = ImageVector.vectorResource(id = R.drawable.ic_top_bar_more_44),
+                contentDescription = "더보기",
+                modifier = Modifier.noRippleClickable(onClick = { onMoreClick() })
+            )
         }
     }
 }
+
 
 @Preview(showBackground = true)
 @Composable
