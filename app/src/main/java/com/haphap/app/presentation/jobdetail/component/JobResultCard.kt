d789cee31d1a6896b3e2b36041c0ed8372526a24
xancxoxin <judy010321@naver.com>
2026-07-09 02:34:26 +0900

    [mod/#34] 전형별 등록 결과 카드 shape 값 변경

--- app/src/main/java/com/haphap/app/presentation/jobdetail/component/JobResultCard.kt
+++ app/src/main/java/com/haphap/app/presentation/jobdetail/component/JobResultCard.kt
@@ -44,7 +44,10 @@ fun JobResultCard(
                 .border(
                     width = 1.dp,
                     color = HapHapTheme.colors.gray100,
-                    shape = RoundedCornerShape(8.dp),
+                    shape = RoundedCornerShape(
+                        bottomStart = 8.dp,
+                        bottomEnd = 8.dp
+                    )
                 )
                 .padding(top = 4.dp, bottom = 5.dp),
             horizontalAlignment = Alignment.CenterHorizontally,
