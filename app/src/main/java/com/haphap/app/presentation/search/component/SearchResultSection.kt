e0633360e3cb78af31b56019400798b6e5df8613
vahkjsdf <vahkjsdf@sookmyung.ac.kr>
2026-07-09 01:56:48 +0900

    [del/#31] 미사용 import 제거

--- app/src/main/java/com/haphap/app/presentation/search/component/SearchResultSection.kt
+++ app/src/main/java/com/haphap/app/presentation/search/component/SearchResultSection.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.grid.GridCells
@@ -17,7 +16,6 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
