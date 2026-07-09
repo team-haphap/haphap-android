0bfe9fdede6e8e763ab3cd0b7f2a2ce06d644b34
vahkjsdf <vahkjsdf@sookmyung.ac.kr>
2026-07-10 08:33:45 +0900

    [feat/#38] 날짜 포맷팅

--- app/src/main/java/com/haphap/app/data/mapper/search/SearchMapper.kt
+++ app/src/main/java/com/haphap/app/data/mapper/search/SearchMapper.kt
@@ -2,9 +2,12 @@ package com.haphap.app.data.mapper.search
 
 import com.haphap.app.data.local.database.RecentSearchEntity
 import com.haphap.app.data.model.search.RecentSearchItemModel
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 fun RecentSearchEntity.toModel(): RecentSearchItemModel = RecentSearchItemModel(
     id = id,
     keyword = searchText,
-    date = searchedAt.toString(),
+    date = SimpleDateFormat("MM.dd", Locale.getDefault()).format(Date(searchedAt)),
 )
