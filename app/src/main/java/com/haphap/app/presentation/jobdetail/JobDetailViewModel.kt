7a26c06dedba5b11608018194f6b3ff68baaa27a
xancxoxin <judy010321@naver.com>
2026-07-09 17:47:40 +0900

    [add/#51] 공고 상세페이지 뷰 모델 구현

---
+++ app/src/main/java/com/haphap/app/presentation/jobdetail/JobDetailViewModel.kt
@@ -0,0 +1,22 @@
+package com.haphap.app.presentation.jobdetail
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import javax.inject.Inject
+
+@HiltViewModel
+class JobDetailViewModel @Inject constructor(
+
+) : ViewModel() {
+
+    private val _uiState = MutableStateFlow(JobDetailContract.State())
+    val uiState = _uiState.asStateFlow()
+
+    fun updateSelectedTab(stage: String) {
+        _uiState.update { it.copy(selectedTab = stage) }
+    }
+
+}
