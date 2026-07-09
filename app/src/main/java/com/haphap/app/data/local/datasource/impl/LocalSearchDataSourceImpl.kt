b36b9c204d3134fc9d2f0c55c399c4a91c50a6d2
vahkjsdf <vahkjsdf@sookmyung.ac.kr>
2026-07-10 07:28:30 +0900

    [feat/#38] LocalSearchDataSource, Impl 구현

---
+++ app/src/main/java/com/haphap/app/data/local/datasource/impl/LocalSearchDataSourceImpl.kt
@@ -0,0 +1,31 @@
+package com.haphap.app.data.local.datasource.impl
+
+import com.haphap.app.data.local.database.RecentSearchDao
+import com.haphap.app.data.local.datasource.api.LocalSearchDataSource
+import com.haphap.app.data.local.database.RecentSearchEntity
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class LocalSearchDataSourceImpl @Inject constructor(
+    private val recentSearchDao: RecentSearchDao,
+) : LocalSearchDataSource {
+
+    override suspend fun setSearchKeyword(searchText: String) {
+        recentSearchDao.removeDuplicatedText(searchText)
+        recentSearchDao.insertRecentItem(RecentSearchEntity(
+            id = 0,
+            searchText = searchText,
+            searchedAt = System.currentTimeMillis()
+        ))
+        recentSearchDao.trimRecentSearchItems()
+    }
+
+    override fun getSearchKeyword(): Flow<List<RecentSearchEntity>> =
+        recentSearchDao.getRecentSearchItems()
+
+
+    override suspend fun deleteSearchKeyword(id: Long) {
+        recentSearchDao.deleteRecentSearchItem(id)
+    }
+
+}
