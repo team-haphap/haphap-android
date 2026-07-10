c4b565ec344fc65029e8cd1bdf26c328da333411
chanmi <cksal1052@ewha.ac.kr>
2026-07-11 06:36:26 +0900

    [feat/#65] uiState / viewmodel 구현

---
+++ app/src/main/java/com/haphap/app/data/model/calendar/CalendarListCardModel.kt
@@ -0,0 +1,9 @@
+package com.haphap.app.data.model.calendar
+
+data class CalendarListCardModel(
+    val id: Long,
+    val titleText: String,
+    val stage: String,
+    val participantCount: Int,
+    val imageUrl: String,
+)
--- app/src/main/java/com/haphap/app/presentation/calendar/CalendarScreen.kt
+++ app/src/main/java/com/haphap/app/presentation/calendar/CalendarScreen.kt
@@ -1,39 +1,44 @@
 package com.haphap.app.presentation.calendar
 
-import android.R.attr.onClick
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.haphap.app.core.designsystem.theme.HapHapTheme
-import com.haphap.app.presentation.calendar.component.CalendarDayItem
+import com.haphap.app.data.model.calendar.CalendarListCardModel
 import com.haphap.app.presentation.calendar.component.CalendarListCardComponent
 import com.haphap.app.presentation.calendar.component.HapHapCustomCalendar
+import kotlinx.collections.immutable.persistentListOf
 import java.time.LocalDate
 
 @Composable
 fun CalendarRoute(
     modifier: Modifier = Modifier,
+    viewModel: CalendarViewModel = hiltViewModel(),
 ) {
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+
     CalendarScreen(
-        onCalendarDateClick = {},
+        uiState = uiState,
+        onCalendarDateClick = viewModel::updateSelectedDate,
         modifier = modifier,
     )
 }
 
 @Composable
 private fun CalendarScreen(
+    uiState: CalendarContract.State,
     onCalendarDateClick: (LocalDate) -> Unit,
     modifier: Modifier = Modifier,
 ) {
@@ -52,12 +57,15 @@ private fun CalendarScreen(
             contentPadding = PaddingValues(vertical = 32.dp),
             verticalArrangement = Arrangement.spacedBy(12.dp),
         ) {
-            items(count = 5) {
+            items(
+                items = uiState.calendarCardList,
+                key = { it.id },
+            ) { card ->
                 CalendarListCardComponent(
-                    titleText = "넹",
-                    stage = "넹",
-                    participantCount = 2,
-                    imageUrl = "",
+                    titleText = card.titleText,
+                    stage = card.stage,
+                    participantCount = card.participantCount,
+                    imageUrl = card.imageUrl,
                 )
             }
         }
@@ -70,6 +78,15 @@ private fun CalendarScreen(
 private fun CalendarScreenPreview() {
     HapHapTheme {
         CalendarScreen(
+            uiState = CalendarContract.State(
+                calendarCardList = persistentListOf(
+                    CalendarListCardModel(1L, "2026 신입 개발자 공개채용", "서류", 32, ""),
+                    CalendarListCardModel(2L, "2026 신입 개발자 공개채용", "서류", 32, ""),
+                    CalendarListCardModel(3L, "2026 신입 개발자 공개채용", "서류", 32, ""),
+                    CalendarListCardModel(4L, "2026 신입 개발자 공개채용", "서류", 32, ""),
+                    CalendarListCardModel(5L, "2026 신입 개발자 공개채용", "서류", 32, ""),
+                ),
+            ),
             onCalendarDateClick = {},
         )
     }
