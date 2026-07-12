53960e5f994612d36609f585d4800c8875bcf57a
vahkjsdf <vahkjsdf@sookmyung.ac.kr>
2026-07-12 23:43:29 +0900

    [chore/#38] line 정리

--- app/src/main/java/com/haphap/app/data/local/datasource/impl/LocalSearchDataSourceImpl.kt
+++ app/src/main/java/com/haphap/app/data/local/datasource/impl/LocalSearchDataSourceImpl.kt
@@ -1,8 +1,8 @@
 package com.haphap.app.data.local.datasource.impl
 
 import com.haphap.app.data.local.database.RecentSearchDao
-import com.haphap.app.data.local.datasource.api.LocalSearchDataSource
 import com.haphap.app.data.local.database.RecentSearchEntity
+import com.haphap.app.data.local.datasource.api.LocalSearchDataSource
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
@@ -12,11 +12,13 @@ class LocalSearchDataSourceImpl @Inject constructor(
 
     override suspend fun setSearchKeyword(searchText: String) {
         recentSearchDao.removeDuplicatedText(searchText)
-        recentSearchDao.insertRecentItem(RecentSearchEntity(
-            id = 0,
-            searchText = searchText,
-            searchedAt = System.currentTimeMillis()
-        ))
+        recentSearchDao.insertRecentItem(
+            RecentSearchEntity(
+                id = 0,
+                searchText = searchText,
+                searchedAt = System.currentTimeMillis()
+            )
+        )
         recentSearchDao.trimRecentSearchItems()
     }
 
