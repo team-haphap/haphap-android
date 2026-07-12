73a5587216b014b915587e7ceb533c009ef2a758
chanmi <cksal1052@ewha.ac.kr>
2026-07-12 16:42:12 +0900

    [refactor/#65] Long -> Int

--- app/src/main/java/com/haphap/app/data/model/calendar/CalendarListCardModel.kt
+++ app/src/main/java/com/haphap/app/data/model/calendar/CalendarListCardModel.kt
@@ -1,7 +1,7 @@
 package com.haphap.app.data.model.calendar
 
 data class CalendarListCardModel(
-    val id: Long,
+    val id: Int,
     val titleText: String,
     val stage: String,
     val participantCount: Int,
