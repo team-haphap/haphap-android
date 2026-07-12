58be3df124f57ce1ea367af061b28fcefa003d9c
jiyoung2ee <mmuscat33@dgu.ac.kr>
2026-07-13 03:30:49 +0900

    [feat/#72] Mapper 구현

--- app/src/main/java/com/haphap/app/data/mapper/home/HomeMapper.kt
+++ app/src/main/java/com/haphap/app/data/mapper/home/HomeMapper.kt
@@ -1,10 +1,19 @@
 package com.haphap.app.data.mapper.home
 
 import com.haphap.app.data.model.home.BannerItemModel
+import com.haphap.app.data.model.home.CountCardModel
 import com.haphap.app.data.remote.dto.home.HomeBannerItemDto
+import com.haphap.app.data.remote.dto.home.HomeTodayResponseDto
 
 fun HomeBannerItemDto.toModel(): BannerItemModel =
     BannerItemModel(
         id = displayOrder,
         imageUrl = imageUrl,
+    )
+
+fun HomeTodayResponseDto.toModel(): CountCardModel =
+    CountCardModel(
+        cumulatedCount = cumulatedCount,
+        onGoingCount = onGoingCount,
+        announcedCount = announcedCount,
     )
