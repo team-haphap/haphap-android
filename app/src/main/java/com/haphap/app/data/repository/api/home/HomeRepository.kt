e813caa7ebace88c7c5a50ccf53f05fc905202de
jiyoung2ee <mmuscat33@dgu.ac.kr>
2026-07-13 03:29:04 +0900

    [feat/#72] getCountCard Repository 추가

--- app/src/main/java/com/haphap/app/data/repository/api/home/HomeRepository.kt
+++ app/src/main/java/com/haphap/app/data/repository/api/home/HomeRepository.kt
@@ -1,7 +1,9 @@
 package com.haphap.app.data.repository.api.home
 
 import com.haphap.app.data.model.home.BannerItemModel
+import com.haphap.app.data.model.home.CountCardModel
 
 interface HomeRepository {
     suspend fun getBannerList(): Result<List<BannerItemModel>>
+    suspend fun getCountCard(): Result<CountCardModel>
 }
