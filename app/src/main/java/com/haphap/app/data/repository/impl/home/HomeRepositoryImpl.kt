da7c45f965ef67610567ec14bdd6a4f4e5c2911c
jiyoung2ee <mmuscat33@dgu.ac.kr>
2026-07-13 02:19:03 +0900

    [feat/#72] RepositoryImpl 구현

---
+++ app/src/main/java/com/haphap/app/data/repository/impl/home/HomeRepositoryImpl.kt
@@ -0,0 +1,24 @@
+package com.haphap.app.data.repository.impl.home
+
+import com.haphap.app.core.util.suspendRunCatching
+import com.haphap.app.data.mapper.home.toModel
+import com.haphap.app.data.model.home.BannerItemModel
+import com.haphap.app.data.remote.datasource.api.home.HomeDataSource
+import com.haphap.app.data.remote.dto.checkData
+import com.haphap.app.data.repository.api.home.HomeRepository
+import jakarta.inject.Inject
+
+class HomeRepositoryImpl @Inject constructor(
+    private val homeDataSource: HomeDataSource,
+) : HomeRepository {
+
+    override suspend fun getBannerList(): Result<List<BannerItemModel>> =
+        suspendRunCatching {
+            homeDataSource.getBannerList()
+                .checkData()
+                .banners
+                .sortedBy { it.displayOrder }
+                .map { it.toModel() }
+        }
+}
+
