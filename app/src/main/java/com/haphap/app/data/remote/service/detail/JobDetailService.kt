5df89e64f72a6d65f6aed5a94f651f43e389e72e
xancxoxin <judy010321@naver.com>
2026-07-13 04:15:50 +0900

    [feat/#73] JobDetailService 추가

---
+++ app/src/main/java/com/haphap/app/data/remote/service/detail/JobDetailService.kt
@@ -0,0 +1,13 @@
+package com.haphap.app.data.remote.service.detail
+
+import com.haphap.app.data.remote.dto.BaseResponse
+import com.haphap.app.data.remote.dto.detail.JobDetailDto
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface JobDetailService {
+    @GET("api/v1/postings/{postingId}/detail")
+    suspend fun getJobPostingDetail(
+        @Path("postingId") postingId: Long,
+    ): BaseResponse<JobDetailDto>
+}
