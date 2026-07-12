c4598c957bd30c03176fa721d8526317d55e917b
vahkjsdf <vahkjsdf@sookmyung.ac.kr>
2026-07-13 01:52:17 +0900

    [feat/#50] 인기 공고 mapper 추가

--- app/src/main/java/com/haphap/app/data/mapper/search/SearchMapper.kt
+++ app/src/main/java/com/haphap/app/data/mapper/search/SearchMapper.kt
@@ -2,6 +2,9 @@ package com.haphap.app.data.mapper.search
 
 import com.haphap.app.data.local.database.RecentSearchEntity
 import com.haphap.app.data.model.search.RecentSearchItemModel
+import com.haphap.app.data.model.search.TrendJobItemModel
+import com.haphap.app.data.remote.dto.search.PopularItemDto
+import com.haphap.app.data.remote.dto.search.PopularListResponseDto
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
@@ -10,4 +13,17 @@ fun RecentSearchEntity.toModel(): RecentSearchItemModel = RecentSearchItemModel(
     id = id,
     keyword = searchText,
     date = SimpleDateFormat("MM.dd", Locale.getDefault()).format(Date(searchedAt)),
+)
+
+
+fun PopularListResponseDto.toModel(): List<TrendJobItemModel> = postings.map { it.toModel() }
+
+fun PopularItemDto.toModel(): TrendJobItemModel = TrendJobItemModel(
+    id = id,
+    imageUrl = imageUrl,
+    category = category,
+    nextStage = nextStage,
+    dDay = daysUntilNextStage,
+    title = title,
+    companyName = companyName,
 )
