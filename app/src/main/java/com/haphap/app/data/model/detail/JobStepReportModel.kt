9e698197d306e775f29ffef69258cda6c2aae4b5
xancxoxin <judy010321@naver.com>
2026-07-13 04:21:27 +0900

    [mod/#73] JobStepReportModel id 추가

--- app/src/main/java/com/haphap/app/data/model/detail/JobStepReportModel.kt
+++ app/src/main/java/com/haphap/app/data/model/detail/JobStepReportModel.kt
@@ -3,7 +3,7 @@ package com.haphap.app.data.model.detail
 import com.haphap.app.presentation.jobdetail.type.JobStepReportType
 
 data class JobStepReportModel(
-    val id: Int,
+    val id: Long,
     val time: String,
     val nickName: String,
     val result: JobStepReportType,
