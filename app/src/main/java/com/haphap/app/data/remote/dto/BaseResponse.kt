fb152f7d3f384e2102257276f27838f8cc016d44
vahkjsdf <vahkjsdf@sookmyung.ac.kr>
2026-07-12 18:22:04 +0900

    [feat/#49] response data null 처리

--- app/src/main/java/com/haphap/app/data/remote/dto/BaseResponse.kt
+++ app/src/main/java/com/haphap/app/data/remote/dto/BaseResponse.kt
@@ -3,6 +3,10 @@ package com.haphap.app.data.remote.dto
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
+private const val HTTP_OK = 200
+private const val HTTP_CREATED = 201
+private const val HTTP_DATA_NULL = 204
+
 @Serializable
 data class BaseResponse<T>(
     @SerialName("status")
@@ -14,3 +18,8 @@ data class BaseResponse<T>(
     @SerialName("data")
     val data: T? = null,
 )
+
+fun <T> BaseResponse<T>.requireData(): T {
+    if (status != HTTP_OK && status != HTTP_CREATED && status != HTTP_DATA_NULL) throw IllegalStateException("API request failed.")
+    return data ?: throw IllegalStateException("Successful response but data was null.")
+}
