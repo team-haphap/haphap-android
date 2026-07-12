6d0795c1ee09ef36df04382707343630f914824d
jiyoung2ee <mmuscat33@dgu.ac.kr>
2026-07-13 04:02:23 +0900

    [feat/#72] Service API 연동

--- app/src/main/java/com/haphap/app/data/remote/service/home/HomeService.kt
+++ app/src/main/java/com/haphap/app/data/remote/service/home/HomeService.kt
@@ -1,6 +1,7 @@
 package com.haphap.app.data.remote.service.home
 
 import com.haphap.app.data.remote.dto.BaseResponse
+import com.haphap.app.data.remote.dto.home.HomeAnnouncementsResponseDto
 import com.haphap.app.data.remote.dto.home.HomeBannerListDto
 import com.haphap.app.data.remote.dto.home.HomeTodayResponseDto
 import retrofit2.http.GET
@@ -11,4 +12,7 @@ interface HomeService {
 
     @GET("api/v1/postings/today")
     suspend fun getCountCard(): BaseResponse<HomeTodayResponseDto>
+
+    @GET("api/v1/postings/announcements")
+    suspend fun getAnnouncements(): BaseResponse<HomeAnnouncementsResponseDto>
 }
