8a527066467c0abc4c8e2f5a417201ce44a9ed8b
jiyoung2ee <mmuscat33@dgu.ac.kr>
2026-07-13 03:30:35 +0900

    [feat/#72] RepositoryImpl 구현

--- app/src/main/java/com/haphap/app/data/repository/impl/home/HomeRepositoryImpl.kt
+++ app/src/main/java/com/haphap/app/data/repository/impl/home/HomeRepositoryImpl.kt
@@ -3,6 +3,7 @@ package com.haphap.app.data.repository.impl.home
 import com.haphap.app.core.util.suspendRunCatching
 import com.haphap.app.data.mapper.home.toModel
 import com.haphap.app.data.model.home.BannerItemModel
+import com.haphap.app.data.model.home.CountCardModel
 import com.haphap.app.data.remote.datasource.api.home.HomeDataSource
 import com.haphap.app.data.remote.dto.checkData
 import com.haphap.app.data.repository.api.home.HomeRepository
@@ -19,6 +20,13 @@ class HomeRepositoryImpl @Inject constructor(
                 .banners
                 .sortedBy { it.displayOrder }
                 .map { it.toModel() }
+        }
+
+    override suspend fun getCountCard(): Result<CountCardModel> =
+        suspendRunCatching {
+            homeDataSource.getCountCard()
+                .checkData()
+                .toModel()
         }
 }
 
