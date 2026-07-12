3e8444bfab484b53f2d54d1b330dbfa24f67cf4e
vahkjsdf <vahkjsdf@sookmyung.ac.kr>
2026-07-13 05:58:12 +0900

    [feat/#50] 자동 완성 service 추가

--- app/src/main/java/com/haphap/app/data/remote/service/SearchService.kt
+++ app/src/main/java/com/haphap/app/data/remote/service/SearchService.kt
@@ -1,10 +1,18 @@
 package com.haphap.app.data.remote.service
 
 import com.haphap.app.data.remote.dto.BaseResponse
+import com.haphap.app.data.remote.dto.search.AutoCompleteListDto
 import com.haphap.app.data.remote.dto.search.PopularListResponseDto
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface SearchService {
     @GET("/api/v1/search/popular")
     suspend fun getPopularList(): BaseResponse<PopularListResponseDto>
+
+    @GET("api/v1/search/autocomplete")
+    suspend fun getAutoCompleteList(
+        @Query("q")
+        q: String,
+    ): BaseResponse<AutoCompleteListDto>
 }
