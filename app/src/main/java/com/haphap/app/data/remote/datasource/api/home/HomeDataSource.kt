17f66045b3f0fc24d05c575b699cf3a8de8f2de9
jiyoung2ee <mmuscat33@dgu.ac.kr>
2026-07-13 03:29:57 +0900

    [feat/#72] DataSource 구현

--- app/src/main/java/com/haphap/app/data/remote/datasource/api/home/HomeDataSource.kt
+++ app/src/main/java/com/haphap/app/data/remote/datasource/api/home/HomeDataSource.kt
@@ -2,7 +2,10 @@ package com.haphap.app.data.remote.datasource.api.home
 
 import com.haphap.app.data.remote.dto.BaseResponse
 import com.haphap.app.data.remote.dto.home.HomeBannerListDto
+import com.haphap.app.data.remote.dto.home.HomeTodayResponseDto
 
 interface HomeDataSource {
     suspend fun getBannerList(): BaseResponse<HomeBannerListDto>
+
+    suspend fun getCountCard(): BaseResponse<HomeTodayResponseDto>
 }
--- app/src/main/java/com/haphap/app/data/remote/datasource/impl/home/HomeDataSourceImpl.kt
+++ app/src/main/java/com/haphap/app/data/remote/datasource/impl/home/HomeDataSourceImpl.kt
@@ -4,13 +4,18 @@ import com.haphap.app.data.remote.datasource.api.home.HomeDataSource
 import com.haphap.app.data.remote.service.home.HomeService
 import com.haphap.app.data.remote.dto.BaseResponse
 import com.haphap.app.data.remote.dto.home.HomeBannerListDto
+import com.haphap.app.data.remote.dto.home.HomeTodayResponseDto
 import jakarta.inject.Inject
 
 class HomeDataSourceImpl @Inject constructor(
-    private val homeBannerService: HomeService,
+    private val homeService: HomeService,
 ) : HomeDataSource {
 
     override suspend fun getBannerList(): BaseResponse<HomeBannerListDto> {
-        return homeBannerService.getBannerList()
+        return homeService.getBannerList()
+    }
+
+    override suspend fun getCountCard(): BaseResponse<HomeTodayResponseDto> {
+        return homeService.getCountCard()
     }
 }
