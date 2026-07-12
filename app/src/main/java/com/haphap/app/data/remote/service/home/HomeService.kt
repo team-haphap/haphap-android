5d846ad7c8bd16edb663cdc68a6e0f6c66208d42
jiyoung2ee <mmuscat33@dgu.ac.kr>
2026-07-13 03:28:04 +0900

    [feat/#72] HomeService CountCard 조회 API 추가

--- app/src/main/java/com/haphap/app/data/remote/service/home/HomeService.kt
+++ app/src/main/java/com/haphap/app/data/remote/service/home/HomeService.kt
@@ -2,9 +2,13 @@ package com.haphap.app.data.remote.service.home
 
 import com.haphap.app.data.remote.dto.BaseResponse
 import com.haphap.app.data.remote.dto.home.HomeBannerListDto
+import com.haphap.app.data.remote.dto.home.HomeTodayResponseDto
 import retrofit2.http.GET
 
 interface HomeService {
     @GET("api/v1/banners")
     suspend fun getBannerList(): BaseResponse<HomeBannerListDto>
+
+    @GET("api/v1/postings/today")
+    suspend fun getCountCard(): BaseResponse<HomeTodayResponseDto>
 }
