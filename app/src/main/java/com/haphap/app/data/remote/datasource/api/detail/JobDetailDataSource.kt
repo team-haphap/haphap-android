144d9e802e74a7aba079aa2144916f456f134486
xancxoxin <judy010321@naver.com>
2026-07-13 16:07:57 +0900

    [feat/#73] JobDetailDataSource에 전형 단계 상태 조회 함수 추가

--- app/src/main/java/com/haphap/app/data/remote/datasource/api/detail/JobDetailDataSource.kt
+++ app/src/main/java/com/haphap/app/data/remote/datasource/api/detail/JobDetailDataSource.kt
@@ -2,7 +2,10 @@ package com.haphap.app.data.remote.datasource.api.detail
 
 import com.haphap.app.data.remote.dto.BaseResponse
 import com.haphap.app.data.remote.dto.detail.JobDetailDto
+import com.haphap.app.data.remote.dto.detail.JobDetailStageStatusListDto
 
 interface JobDetailDataSource {
-    suspend fun getJobPostingDetail(postingId: Long): BaseResponse<JobDetailDto>
+    suspend fun getJobPostingDetail(postingId: Int): BaseResponse<JobDetailDto>
+
+    suspend fun getJobPostingStageStatuses(postingId: Int): BaseResponse<JobDetailStageStatusListDto>
 }
