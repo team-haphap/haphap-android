5c50023eee91014bd88facb3f6c8ac0859fb7bf7
xancxoxin <judy010321@naver.com>
2026-07-14 02:27:36 +0900

    [feat/#73] JobDetailRepositoryImpl에 공고 조회 기록 구현 추가

--- app/src/main/java/com/haphap/app/data/repository/impl/detail/JobDetailRepositoryImpl.kt
+++ app/src/main/java/com/haphap/app/data/repository/impl/detail/JobDetailRepositoryImpl.kt
@@ -61,6 +61,11 @@ class JobDetailRepositoryImpl @Inject constructor(
             response.message
         }
 
+    override suspend fun recordView(postingId: Int): Result<Unit> =
+        suspendRunCatching {
+            jobDetailDataSource.recordView(postingId)
+        }
+
     companion object {
         private const val HTTP_OK = 200
     }
