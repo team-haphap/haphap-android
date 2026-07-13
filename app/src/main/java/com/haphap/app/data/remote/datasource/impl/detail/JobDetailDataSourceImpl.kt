3e7b721ba46a4b75353bca314e3b3f422b4ecb67
xancxoxin <judy010321@naver.com>
2026-07-13 18:32:26 +0900

    [feat/#73] JobDetailDataSourceImpl에 전형 목록 조회 구현 추가

--- app/src/main/java/com/haphap/app/data/remote/datasource/impl/detail/JobDetailDataSourceImpl.kt
+++ app/src/main/java/com/haphap/app/data/remote/datasource/impl/detail/JobDetailDataSourceImpl.kt
@@ -3,6 +3,7 @@ package com.haphap.app.data.remote.datasource.impl.detail
 import com.haphap.app.data.remote.datasource.api.detail.JobDetailDataSource
 import com.haphap.app.data.remote.dto.BaseResponse
 import com.haphap.app.data.remote.dto.detail.JobDetailDto
+import com.haphap.app.data.remote.dto.detail.JobDetailStageListDto
 import com.haphap.app.data.remote.dto.detail.JobDetailStageStatusListDto
 import com.haphap.app.data.remote.service.detail.JobDetailService
 import jakarta.inject.Inject
@@ -13,6 +14,9 @@ class JobDetailDataSourceImpl @Inject constructor(
 
     override suspend fun getJobPostingDetail(postingId: Int): BaseResponse<JobDetailDto> =
         jobDetailService.getJobPostingDetail(postingId)
+
+    override suspend fun getJobPostingStages(postingId: Int): BaseResponse<JobDetailStageListDto> =
+        jobDetailService.getJobPostingStages(postingId)
 
     override suspend fun getJobPostingStageStatuses(postingId: Int): BaseResponse<JobDetailStageStatusListDto> =
         jobDetailService.getJobPostingStageStatuses(postingId)
