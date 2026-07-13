45d19d6b0b2951e988a271512b5d7b792e336537
Hiimynameiss <sh2410152@sookmyung.ac.kr>
2026-07-13 17:01:20 +0900

    [mod/#76] NotificationChannelType 서버에 맞게 수정

--- app/src/main/java/com/haphap/app/presentation/register/type/NotificationChannelType.kt
+++ app/src/main/java/com/haphap/app/presentation/register/type/NotificationChannelType.kt
@@ -1,8 +1,8 @@
 package com.haphap.app.presentation.register.type
 
 enum class NotificationChannelType(val text: String) {
-    SMS("문자"),
     EMAIL("이메일"),
-    CALL("전화"),
-    WEB("기업 홈페이지"),
+    SMS("문자"),
+    PAGE("기업 홈페이지"),
+    PHONE_CALL("전화"),
 }
