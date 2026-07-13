e83abc20b2f946b0545181365f53dad454f80a77
xancxoxin <judy010321@naver.com>
2026-07-14 02:27:17 +0900

    [feat/#73] JobDetailDataSourceImpl에 공고 조회 기록 구현 추가

--- app/src/main/java/com/haphap/app/data/remote/datasource/impl/detail/JobDetailDataSourceImpl.kt
+++ app/src/main/java/com/haphap/app/data/remote/datasource/impl/detail/JobDetailDataSourceImpl.kt
@@ -30,4 +30,7 @@ class JobDetailDataSourceImpl @Inject constructor(
 
     override suspend fun deleteAlarms(postingId: Int): BaseResponse<Unit> =
         jobDetailService.deleteAlarms(postingId)
+
+    override suspend fun recordView(postingId: Int) =
+        jobDetailService.recordView(postingId)
 }
