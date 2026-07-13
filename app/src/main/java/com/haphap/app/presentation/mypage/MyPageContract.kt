54851ee1d596cb88f16c212a9351412e8e35fff0
chanmi <cksal1052@ewha.ac.kr>
2026-07-13 23:50:10 +0900

    [feat/#79] 마이페이지 API 연동

--- app/src/main/java/com/haphap/app/presentation/mypage/MyPageContract.kt
+++ app/src/main/java/com/haphap/app/presentation/mypage/MyPageContract.kt
@@ -1,6 +1,7 @@
 package com.haphap.app.presentation.mypage
 
 import androidx.compose.runtime.Immutable
+import com.haphap.app.data.model.mypage.MyPageModel
 
 sealed interface MyPageContract {
     @Immutable
@@ -9,5 +10,17 @@ sealed interface MyPageContract {
         val nameText: String = "",
         val nickNameText: String = "",
         val emailText: String = "",
+        val myPageUiState: MyPageUiState<MyPageModel> = MyPageUiState.Idle,
     )
+}
+
+sealed interface MyPageUiState<out T> {
+    data object Idle : MyPageUiState<Nothing>
+    data object Loading : MyPageUiState<Nothing>
+    data class Success<T>(
+        val data: T,
+    ) : MyPageUiState<T>
+    data class Failure(
+        val msg: String,
+    ) : MyPageUiState<Nothing>
 }
--- app/src/main/java/com/haphap/app/presentation/mypage/MyPageViewModel.kt
+++ app/src/main/java/com/haphap/app/presentation/mypage/MyPageViewModel.kt
@@ -1,16 +1,54 @@
 package com.haphap.app.presentation.mypage
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.haphap.app.data.repository.api.mypage.MyPageRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import timber.log.Timber
 import javax.inject.Inject
 
 @HiltViewModel
 class MyPageViewModel @Inject constructor(
-
+    private val myPageRepository: MyPageRepository,
 ) : ViewModel() {
     private val _uiState = MutableStateFlow(MyPageContract.State())
     val uiState: StateFlow<MyPageContract.State> = _uiState.asStateFlow()
+
+    init {
+        myPage()
+    }
+
+    fun myPage() {
+        _uiState.update { it.copy(myPageUiState = MyPageUiState.Loading) }
+
+        viewModelScope.launch {
+            myPageRepository.getMyPage()
+                .onSuccess { model ->
+                    _uiState.update {
+                        it.copy(
+                            myPageUiState = MyPageUiState.Success(model),
+                            profileUrl = model.profileImageUrl,
+                            nameText = model.name,
+                            nickNameText = model.anonymousName,
+                            emailText = model.email,
+                        )
+                    }
+                }
+                .onFailure { throwable ->
+                    Timber.e(throwable)
+                    _uiState.update {
+                        it.copy(
+                            myPageUiState = MyPageUiState.Failure(
+                                throwable.message ?: "오류"
+                            )
+                        )
+                    }
+                }
+        }
+    }
 }
