623d538ce5ab8b2fbbd7162bcacda75db9bb1964
chanmi <cksal1052@ewha.ac.kr>
2026-07-14 01:20:06 +0900

    [feat/#81] viewcount api 제작

---
+++ app/src/main/java/com/haphap/app/data/remote/datasource/impl/viewcount/ViewCountDataSourceImpl.kt
@@ -0,0 +1,14 @@
+package com.haphap.app.data.remote.datasource.impl.viewcount
+
+import com.haphap.app.data.remote.datasource.api.viewcount.ViewCountDataSource
+import com.haphap.app.data.remote.dto.BaseResponse
+import com.haphap.app.data.remote.service.ViewCountService
+import javax.inject.Inject
+
+class ViewCountDataSourceImpl @Inject constructor(
+    private val viewCountService: ViewCountService,
+) : ViewCountDataSource {
+    override suspend fun patchViewCount(postingId: Int): BaseResponse<Unit> {
+        return viewCountService.patchViewCount(postingId)
+    }
+}
