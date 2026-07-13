aa3d75df32af4986f084b02c84771705ddf81f21
xancxoxin <judy010321@naver.com>
2026-07-13 16:08:42 +0900

    [feat/#73] JobDetailRepositoryImpl에 전형 단계 상태 조회 구현 추가

--- app/src/main/java/com/haphap/app/data/repository/impl/detail/JobDetailRepositoryImpl.kt
+++ app/src/main/java/com/haphap/app/data/repository/impl/detail/JobDetailRepositoryImpl.kt
@@ -2,20 +2,30 @@ package com.haphap.app.data.repository.impl.detail
 
 import com.haphap.app.core.util.suspendRunCatching
 import com.haphap.app.data.mapper.detail.toJobDetailModel
+import com.haphap.app.data.mapper.detail.toJobStepModels
 import com.haphap.app.data.model.detail.JobDetailModel
+import com.haphap.app.data.model.detail.JobStepModel
 import com.haphap.app.data.remote.datasource.api.detail.JobDetailDataSource
 import com.haphap.app.data.remote.dto.checkData
 import com.haphap.app.data.repository.api.detail.JobDetailRepository
 import javax.inject.Inject
+import kotlinx.collections.immutable.ImmutableList
 
 class JobDetailRepositoryImpl @Inject constructor(
     private val jobDetailDataSource: JobDetailDataSource,
 ) : JobDetailRepository {
 
-    override suspend fun getJobPostingDetail(postingId: Long): Result<JobDetailModel> =
+    override suspend fun getJobPostingDetail(postingId: Int): Result<JobDetailModel> =
         suspendRunCatching {
             jobDetailDataSource.getJobPostingDetail(postingId)
                 .checkData()
                 .toJobDetailModel()
+        }
+
+    override suspend fun getJobPostingStageStatuses(postingId: Int): Result<ImmutableList<JobStepModel>> =
+        suspendRunCatching {
+            jobDetailDataSource.getJobPostingStageStatuses(postingId)
+                .checkData()
+                .toJobStepModels()
         }
 }
