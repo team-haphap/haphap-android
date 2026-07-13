a461d7db861a8d352b2f1182fc77154d41eae3ef
xancxoxin <judy010321@naver.com>
2026-07-13 16:11:57 +0900

    [mod/#73] postingId Int로 통일

--- app/src/main/java/com/haphap/app/data/model/detail/JobStepReportModel.kt
+++ app/src/main/java/com/haphap/app/data/model/detail/JobStepReportModel.kt
@@ -3,7 +3,7 @@ package com.haphap.app.data.model.detail
 import com.haphap.app.presentation.jobdetail.type.JobStepReportType
 
 data class JobStepReportModel(
-    val id: Long,
+    val id: Int,
     val time: String,
     val nickName: String,
     val result: JobStepReportType,
