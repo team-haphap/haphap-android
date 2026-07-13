4a54522a5841c7c52ec8b6ae1538aeb709ae6803
jiyoung2ee <mmuscat33@dgu.ac.kr>
2026-07-13 23:51:45 +0900

    [del/#72] 불필요한 import 수정

--- app/src/main/java/com/haphap/app/data/remote/datasource/impl/home/HomeDataSourceImpl.kt
+++ app/src/main/java/com/haphap/app/data/remote/datasource/impl/home/HomeDataSourceImpl.kt
@@ -3,7 +3,6 @@ package com.haphap.app.data.remote.datasource.impl.home
 import com.haphap.app.data.remote.datasource.api.home.HomeDataSource
 import com.haphap.app.data.remote.service.home.HomeService
 import com.haphap.app.data.remote.dto.BaseResponse
-import com.haphap.app.data.remote.dto.checkData
 import com.haphap.app.data.remote.dto.home.HomeAnnouncementsResponseDto
 import com.haphap.app.data.remote.dto.home.HomeBannerListDto
 import com.haphap.app.data.remote.dto.home.HomePostingsResponseDto
