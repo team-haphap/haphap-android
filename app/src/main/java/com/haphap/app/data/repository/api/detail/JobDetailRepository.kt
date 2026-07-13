df127f46399979c8840c905838d26175671195a1
xancxoxin <judy010321@naver.com>
2026-07-13 16:08:31 +0900

    [feat/#73] JobDetailRepository에 전형 단계 상태 조회 함수 추가

--- app/src/main/java/com/haphap/app/data/repository/api/detail/JobDetailRepository.kt
+++ app/src/main/java/com/haphap/app/data/repository/api/detail/JobDetailRepository.kt
@@ -1,7 +1,11 @@
 package com.haphap.app.data.repository.api.detail
 
 import com.haphap.app.data.model.detail.JobDetailModel
+import com.haphap.app.data.model.detail.JobStepModel
+import kotlinx.collections.immutable.ImmutableList
 
 interface JobDetailRepository {
-    suspend fun getJobPostingDetail(postingId: Long): Result<JobDetailModel>
+    suspend fun getJobPostingDetail(postingId: Int): Result<JobDetailModel>
+
+    suspend fun getJobPostingStageStatuses(postingId: Int): Result<ImmutableList<JobStepModel>>
 }
