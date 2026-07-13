47152a0064f330e57914544a9fa489f62eadcfb7
xancxoxin <judy010321@naver.com>
2026-07-13 18:32:16 +0900

    [feat/#73] JobDetailDataSource에 전형 목록 조회 함수 추가

--- app/src/main/java/com/haphap/app/data/remote/datasource/api/detail/JobDetailDataSource.kt
+++ app/src/main/java/com/haphap/app/data/remote/datasource/api/detail/JobDetailDataSource.kt
@@ -2,10 +2,13 @@ package com.haphap.app.data.remote.datasource.api.detail
 
 import com.haphap.app.data.remote.dto.BaseResponse
 import com.haphap.app.data.remote.dto.detail.JobDetailDto
+import com.haphap.app.data.remote.dto.detail.JobDetailStageListDto
 import com.haphap.app.data.remote.dto.detail.JobDetailStageStatusListDto
 
 interface JobDetailDataSource {
     suspend fun getJobPostingDetail(postingId: Int): BaseResponse<JobDetailDto>
+
+    suspend fun getJobPostingStages(postingId: Int): BaseResponse<JobDetailStageListDto>
 
     suspend fun getJobPostingStageStatuses(postingId: Int): BaseResponse<JobDetailStageStatusListDto>
 }
