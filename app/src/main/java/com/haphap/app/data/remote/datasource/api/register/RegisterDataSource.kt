4ee4336540fe4e9a7e6fb2b3f99f43f6a5084a6a
Hiimynameiss <sh2410152@sookmyung.ac.kr>
2026-07-13 17:20:11 +0900

    [feat/#76] 상태 등록 DataSource 구현

--- app/src/main/java/com/haphap/app/data/remote/datasource/api/register/RegisterDataSource.kt
+++ app/src/main/java/com/haphap/app/data/remote/datasource/api/register/RegisterDataSource.kt
@@ -2,9 +2,12 @@ package com.haphap.app.data.remote.datasource.api.register
 
 import com.haphap.app.data.remote.dto.BaseResponse
 import com.haphap.app.data.remote.dto.register.RegisterNameListResponseDto
+import com.haphap.app.data.remote.dto.register.RegisterRequestDto
 import com.haphap.app.data.remote.dto.register.RegisterStageListResponseDto
+import com.haphap.app.data.remote.dto.register.RegistrationResponseDto
 
 interface RegisterDataSource {
     suspend fun getPostingNames(): BaseResponse<RegisterNameListResponseDto>
     suspend fun getPostingStages(postingId: Int): BaseResponse<RegisterStageListResponseDto>
+    suspend fun postRegistration(request: RegisterRequestDto): BaseResponse<RegistrationResponseDto>
 }
--- app/src/main/java/com/haphap/app/data/remote/datasource/impl/register/RegisterDataSourceImpl.kt
+++ app/src/main/java/com/haphap/app/data/remote/datasource/impl/register/RegisterDataSourceImpl.kt
@@ -3,7 +3,9 @@ package com.haphap.app.data.remote.datasource.impl.register
 import com.haphap.app.data.remote.datasource.api.register.RegisterDataSource
 import com.haphap.app.data.remote.dto.BaseResponse
 import com.haphap.app.data.remote.dto.register.RegisterNameListResponseDto
+import com.haphap.app.data.remote.dto.register.RegisterRequestDto
 import com.haphap.app.data.remote.dto.register.RegisterStageListResponseDto
+import com.haphap.app.data.remote.dto.register.RegistrationResponseDto
 import com.haphap.app.data.remote.service.register.RegisterService
 import jakarta.inject.Inject
 
@@ -16,5 +18,9 @@ class RegisterDataSourceImpl @Inject constructor(
 
     override suspend fun getPostingStages(postingId: Int): BaseResponse<RegisterStageListResponseDto> {
         return postingService.getPostingStages(postingId)
+    }
+
+    override suspend fun postRegistration(request: RegisterRequestDto): BaseResponse<RegistrationResponseDto> {
+        return postingService.postRegistration(request)
     }
 }
