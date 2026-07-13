77ee9ad7962c268670d1cbc00bffe7879cd99149
vahkjsdf <vahkjsdf@sookmyung.ac.kr>
2026-07-13 21:03:26 +0900

    [feat/#50] 검색 결과 datasource, impl 추가

--- app/src/main/java/com/haphap/app/data/remote/datasource/api/search/SearchDataSource.kt
+++ app/src/main/java/com/haphap/app/data/remote/datasource/api/search/SearchDataSource.kt
@@ -2,7 +2,14 @@ package com.haphap.app.data.remote.datasource.api.search
 
 import com.haphap.app.data.remote.dto.BaseResponse
 import com.haphap.app.data.remote.dto.search.PopularListResponseDto
+import com.haphap.app.data.remote.dto.search.SearchResultListResponseDto
 
 interface SearchDataSource {
     suspend fun getPopularList(): BaseResponse<PopularListResponseDto>
+    suspend fun getSearchResultList(
+        q: String?,
+        category: List<String>?,
+        page: Int?,
+        size: Int?,
+    ): BaseResponse<SearchResultListResponseDto>
 }
--- app/src/main/java/com/haphap/app/data/remote/datasource/impl/search/SearchDataSourceImpl.kt
+++ app/src/main/java/com/haphap/app/data/remote/datasource/impl/search/SearchDataSourceImpl.kt
@@ -3,6 +3,7 @@ package com.haphap.app.data.remote.datasource.impl.search
 import com.haphap.app.data.remote.datasource.api.search.SearchDataSource
 import com.haphap.app.data.remote.dto.BaseResponse
 import com.haphap.app.data.remote.dto.search.PopularListResponseDto
+import com.haphap.app.data.remote.dto.search.SearchResultListResponseDto
 import com.haphap.app.data.remote.service.SearchService
 import javax.inject.Inject
 
@@ -12,5 +13,19 @@ class SearchDataSourceImpl @Inject constructor(
 
     override suspend fun getPopularList(): BaseResponse<PopularListResponseDto> {
         return searchService.getPopularList()
+    }
+
+    override suspend fun getSearchResultList(
+        q: String?,
+        category: List<String>?,
+        page: Int?,
+        size: Int?,
+    ): BaseResponse<SearchResultListResponseDto> {
+        return searchService.getSearchResultList(
+            q = q,
+            category = category,
+            page = page,
+            size = size,
+        )
     }
 }
