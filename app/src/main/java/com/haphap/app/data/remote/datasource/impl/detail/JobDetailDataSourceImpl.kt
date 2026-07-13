c7edfcebd99069c57ef242e97554f9382c800364
xancxoxin <judy010321@naver.com>
2026-07-13 16:08:09 +0900

    [feat/#73] JobDetailDataSourceImpl에 전형 단계 상태 조회 구현 추가

--- app/src/main/java/com/haphap/app/data/remote/datasource/impl/detail/JobDetailDataSourceImpl.kt
+++ app/src/main/java/com/haphap/app/data/remote/datasource/impl/detail/JobDetailDataSourceImpl.kt
@@ -3,6 +3,7 @@ package com.haphap.app.data.remote.datasource.impl.detail
 import com.haphap.app.data.remote.datasource.api.detail.JobDetailDataSource
 import com.haphap.app.data.remote.dto.BaseResponse
 import com.haphap.app.data.remote.dto.detail.JobDetailDto
+import com.haphap.app.data.remote.dto.detail.JobDetailStageStatusListDto
 import com.haphap.app.data.remote.service.detail.JobDetailService
 import jakarta.inject.Inject
 
@@ -10,6 +11,9 @@ class JobDetailDataSourceImpl @Inject constructor(
     private val jobDetailService: JobDetailService,
 ) : JobDetailDataSource {
 
-    override suspend fun getJobPostingDetail(postingId: Long): BaseResponse<JobDetailDto> =
+    override suspend fun getJobPostingDetail(postingId: Int): BaseResponse<JobDetailDto> =
         jobDetailService.getJobPostingDetail(postingId)
+
+    override suspend fun getJobPostingStageStatuses(postingId: Int): BaseResponse<JobDetailStageStatusListDto> =
+        jobDetailService.getJobPostingStageStatuses(postingId)
 }
