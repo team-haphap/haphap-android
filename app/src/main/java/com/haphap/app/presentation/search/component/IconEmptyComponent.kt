894de91db785b99d4d95ea46a1bebdb8700bc14c
vahkjsdf <vahkjsdf@sookmyung.ac.kr>
2026-07-14 05:55:51 +0900

    [del/#50] 미사용 import 제거

--- app/src/main/java/com/haphap/app/presentation/search/component/IconEmptyComponent.kt
+++ app/src/main/java/com/haphap/app/presentation/search/component/IconEmptyComponent.kt
@@ -1,11 +1,8 @@
 package com.haphap.app.presentation.search.component
 
-import android.R.attr.text
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
