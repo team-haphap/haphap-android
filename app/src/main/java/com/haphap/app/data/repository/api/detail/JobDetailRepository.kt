729a364835b91a130eed93dedea93ab240b890fb
xancxoxin <judy010321@naver.com>
2026-07-13 18:32:40 +0900

    [feat/#73] JobDetailRepository에 전형 목록 조회 함수 추가

--- app/src/main/java/com/haphap/app/data/repository/api/detail/JobDetailRepository.kt
+++ app/src/main/java/com/haphap/app/data/repository/api/detail/JobDetailRepository.kt
@@ -1,11 +1,14 @@
 package com.haphap.app.data.repository.api.detail
 
 import com.haphap.app.data.model.detail.JobDetailModel
+import com.haphap.app.data.model.detail.JobResultTabModel
 import com.haphap.app.data.model.detail.JobStepModel
 import kotlinx.collections.immutable.ImmutableList
 
 interface JobDetailRepository {
     suspend fun getJobPostingDetail(postingId: Int): Result<JobDetailModel>
+
+    suspend fun getJobPostingStages(postingId: Int): Result<ImmutableList<JobResultTabModel>>
 
     suspend fun getJobPostingStageStatuses(postingId: Int): Result<ImmutableList<JobStepModel>>
 }
