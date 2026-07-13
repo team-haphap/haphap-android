3085308a84cb5255fdc04b5c7f3e5915601f4236
xancxoxin <judy010321@naver.com>
2026-07-13 16:07:48 +0900

    [feat/#73] JobDetailService에 전형 단계 상태 조회 API 추가

--- app/src/main/java/com/haphap/app/data/remote/service/detail/JobDetailService.kt
+++ app/src/main/java/com/haphap/app/data/remote/service/detail/JobDetailService.kt
@@ -2,12 +2,18 @@ package com.haphap.app.data.remote.service.detail
 
 import com.haphap.app.data.remote.dto.BaseResponse
 import com.haphap.app.data.remote.dto.detail.JobDetailDto
+import com.haphap.app.data.remote.dto.detail.JobDetailStageStatusListDto
 import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface JobDetailService {
     @GET("api/v1/postings/{postingId}/detail")
     suspend fun getJobPostingDetail(
-        @Path("postingId") postingId: Long,
+        @Path("postingId") postingId: Int,
     ): BaseResponse<JobDetailDto>
+
+    @GET("api/v1/postings/{postingId}/statistics")
+    suspend fun getJobPostingStageStatuses(
+        @Path("postingId") postingId: Int,
+    ): BaseResponse<JobDetailStageStatusListDto>
 }
