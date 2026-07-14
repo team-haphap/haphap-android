d811f20a3cc534153011476a19159d4bb80bb2a2
Hiimynameiss <sh2410152@sookmyung.ac.kr>
2026-07-14 22:43:03 +0900

    [del/#76] 불필요한 import 제거

--- app/src/main/java/com/haphap/app/presentation/register/RegisterViewModel.kt
+++ app/src/main/java/com/haphap/app/presentation/register/RegisterViewModel.kt
@@ -1,6 +1,5 @@
 package com.haphap.app.presentation.register
 
-import android.R.id.message
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
