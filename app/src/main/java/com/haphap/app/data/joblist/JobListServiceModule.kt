42814fae2accf24acdd28f4d047e4371cc3e1e7a
vahkjsdf <vahkjsdf@sookmyung.ac.kr>
2026-07-14 16:41:25 +0900

    [feat/#77] 공고 리스트 di 추가

---
+++ app/src/main/java/com/haphap/app/data/joblist/JobListServiceModule.kt
@@ -0,0 +1,20 @@
+package com.haphap.app.data.joblist
+
+import com.haphap.app.data.remote.service.home.HomeService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.create
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object JobListServiceModule {
+    @Provides
+    @Singleton
+    fun provideHomeService(
+        retrofit: Retrofit
+    ): HomeService = retrofit.create()
+}
