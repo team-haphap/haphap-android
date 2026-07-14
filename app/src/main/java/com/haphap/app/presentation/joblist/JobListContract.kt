5c446929b541c7a88823721e35d2fcc05e1e5437
vahkjsdf <vahkjsdf@sookmyung.ac.kr>
2026-07-14 16:50:05 +0900

    [feat/#77] JobListUiState 추가

--- app/src/main/java/com/haphap/app/presentation/joblist/JobListContract.kt
+++ app/src/main/java/com/haphap/app/presentation/joblist/JobListContract.kt
@@ -3,6 +3,7 @@ package com.haphap.app.presentation.joblist
 import androidx.compose.runtime.Immutable
 import com.haphap.app.data.model.list.JobItemModel
 import com.haphap.app.presentation.common.state.CategoryChipState
+import com.haphap.app.presentation.jobdetail.JobDetailUiState
 import kotlinx.collections.immutable.ImmutableList
 import kotlinx.collections.immutable.persistentListOf
 
@@ -12,5 +13,16 @@ sealed interface JobListContract {
     data class State(
         val categoryChipState: CategoryChipState = CategoryChipState(),
         val jobList: ImmutableList<JobItemModel> = persistentListOf(),
+        val jobListUiState: JobListUiState = JobListUiState.Idle,
     )
+}
+
+sealed interface JobListUiState {
+    data object Idle : JobListUiState
+    data object Loading : JobListUiState
+    data object Empty : JobListUiState
+    data object Success : JobListUiState
+    data class Failure(
+        val msg: String,
+    ) : JobListUiState
 }
