1611ae1385842342ea390920d07a176976164c4c
Hiimynameiss <sh2410152@sookmyung.ac.kr>
2026-07-16 05:36:04 +0900

    [fix/#93] 로그인 이동 이상한 거 해결

---
+++ app/src/main/java/com/haphap/app/presentation/auth/login/LoginContract.kt
@@ -0,0 +1,22 @@
+package com.haphap.app.presentation.auth.login
+
+import androidx.compose.runtime.Immutable
+
+sealed interface LoginContract {
+    @Immutable
+    data class State(
+        val loginUiState: LoginUiState = LoginUiState.Idle,
+    )
+
+    sealed class SideEffect {
+        data class NavigateToSignUpComplete(val userName: String) : SideEffect()
+        data class OnShowToast(val message: String) : SideEffect()
+    }
+}
+
+sealed interface LoginUiState {
+    data object Idle : LoginUiState
+    data object Loading : LoginUiState
+    data object Success : LoginUiState
+    data object Failure : LoginUiState
+}
