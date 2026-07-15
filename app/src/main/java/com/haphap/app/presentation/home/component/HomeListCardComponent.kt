6d08acf0071a0ee2d9ea0dab6e2fc2e6c8d8971f
jiyoung2ee <mmuscat33@dgu.ac.kr>
2026-07-16 03:04:30 +0900

    [fix/#92] [] 삭제 및 이미지 RoundedCornerShape 수정

--- app/src/main/java/com/haphap/app/presentation/home/component/HomeListCardComponent.kt
+++ app/src/main/java/com/haphap/app/presentation/home/component/HomeListCardComponent.kt
@@ -84,7 +84,7 @@ fun HomeListCardComponent(
             )
 
             Text(
-                text = "[$stageName] 발표 예상",
+                text = "$stageName 발표 예상",
                 style = HapHapTheme.typography.caption.r10,
                 color = HapHapTheme.colors.gray600,
             )
@@ -108,7 +108,7 @@ private fun HomeListCardImage(
 ) {
     Box(
         modifier = modifier
-            .clip(shape = RoundedCornerShape(6.dp))
+            .clip(shape = RoundedCornerShape(8.dp))
             .background(HapHapTheme.colors.white)
             .border(
                 color = HapHapTheme.colors.gray100,
